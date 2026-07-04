dff354ac850e3f3b8af998a1cc9903b4c4dace04
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-04 12:17:20 +0530

    Time: 4 ms (63.67%), Space: 18.8 MB (93.96%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int V= numCourses;
+        vector<int> graph[V];
+
+        for(auto &it:prerequisites){
+            graph[it[1]].push_back(it[0]);
+        }
+
+        vector<int> indegree(V,0);
+
+        for(int i=0;i<V;i++){
+            for(auto &ele: graph[i]){
+                indegree[ele]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> res;
+
+        while(!q.empty()){
+            int ele=q.front();
+            q.pop();
+
+            res.push_back(ele);
+
+            for(auto neighbour:graph[ele]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.push(neighbour);
+                }
+            }
+        }
+        if(res.size()!=V){
+            return false;
+        }
+        return true;
+        
+        
+    }
+};
