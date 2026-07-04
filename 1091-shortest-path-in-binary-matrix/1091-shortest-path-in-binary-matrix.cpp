5f01da6adbb605b19f465e517d9dd70be2c3dbd2
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-04 14:35:20 +0530

    Time: 27 ms (32.44%), Space: 21.5 MB (99.99%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+
+        int delrow[]= {-1,-1,0,1,1,1,0,-1};
+        int delcol[]= {0,1,1,1,0,-1,-1,-1};
+
+        if(grid[0][0]!=0 || grid[n-1][n-1]!=0){
+            return -1;
+        }
+
+       priority_queue< pair<int,pair<int,int>>,
+       vector<pair<int,pair<int,int>>> ,
+       greater<pair<int,pair<int,int>>>> pq;
+        pq.push({1,{0,0}});
+        grid[0][0]=1;
+
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int i=pq.top().second.first;
+            int j= pq.top().second.second;
+            pq.pop();
+
+            if(i==n-1 && j==n-1){
+                return dis;
+            }
+
+            for(int k=0;k<8;k++){
+                int row= delrow[k]+i;
+                int col= delcol[k]+j;
+
+                if(row<n && row>=0 && col<n && col>=0){
+                    if(grid[row][col]==0){
+                       grid[row][col]=1;
+                        pq.push({dis+1,{row,col}});
+                    }
+                }
+            }
+        }
+
+        return -1;
+        
+
+
+        
+    }
+};
