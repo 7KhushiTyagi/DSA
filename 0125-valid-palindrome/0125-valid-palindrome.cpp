26f2511cabc7eb8fea4706eba8f5049d57f9cce1
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-11 14:15:47 +0530

    Time: 0 ms (100%), Space: 10 MB (55.13%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0;
+        int right=s.size()-1;
+
+        while(right>left){
+            if(!isalnum(s[left])){
+                left++;
+            }else if(!isalnum(s[right])){
+                right--;
+            }
+            else{
+                if(tolower(s[left])!=tolower(s[right])){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+
+        
+    }
+};
