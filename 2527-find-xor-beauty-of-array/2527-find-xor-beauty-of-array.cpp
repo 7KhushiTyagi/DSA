54675fa2bffea29d28a4bb0de01a08f46d5b221e
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-12 19:10:59 +0530

    Time: 0 ms (100%), Space: 43.6 MB (74.23%) - LeetHub

---
+++ 2527-find-xor-beauty-of-array/2527-find-xor-beauty-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums) {
+        int res=0;
+
+        for(int num:nums){
+            res^=num;
+        }
+        return res;
+        
+    }
+};
