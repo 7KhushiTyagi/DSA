5efb0da0c8fec5a224d74af10cea23b796d33f49
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-21 11:24:34 +0530

    Time: 0 ms (100%), Space: 8.1 MB (18.29%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -6,11 +6,10 @@ public:
         dp[1]=1;
 
         for(int i=2;i<=n;i++){
-            for (int j = 1; j <= i; ++j) {
-            dp[i] += dp[j - 1] * dp[i - j];
-        }
+            for(int j=1;j<=i;j++){
+                dp[i]+= dp[j-1]*dp[i-j];
+            }
         }
         return dp[n];
-        
     }
 };
