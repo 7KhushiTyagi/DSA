fb6567a391c5848651c904d30a1792e2097f6fff
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-21 11:19:17 +0530

    Time: 3 ms (69.92%), Space: 15.5 MB (65.12%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,28 @@
-void helper(vector<string> &res,string curr,int n,int open,int close){
-
-    if(curr.size()==2*n){
-        res.push_back(curr);
-        return;
-    }
-    if(open<n){
-        helper(res,curr+"(",n,open+1,close);
-    }
-    if(close<open){
-        helper(res,curr+")",n,open,close+1);
-    }
-}
-
-
 class Solution {
 public:
+    void helper(int n,int open,int close,vector<string>& res,string curr){
+        
+
+        if(curr.size()==2*n){
+            res.push_back(curr);
+            return;
+        }
+
+        if(open<n){
+            helper(n,open+1,close,res,curr+'(');
+        }
+        if(close<open){
+            helper(n,open,close+1,res,curr+')');
+        }
+
+
+    }
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-         helper(res,"",n,0,0);
-         return res;
+        string curr;
+
+        helper(n,0,0,res,curr);
+        return res;
         
     }
 };
