9b01066a361274d260f704e00ae5f19e9af441e5
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-22 13:20:23 +0530

    Time: 6 ms (66.23%), Space: 11.9 MB (45.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int left=0;
-        int maxi=0;
         unordered_map<char,int> map;
+        
+        int left=0;
+        int len=0;
 
         for(int right=0;right<s.length();right++){
             if(map.find(s[right])!=map.end()){
                 left= max(left,map[s[right]]+1);
             }
-            maxi=max(maxi,right-left+1);
+            len=max(len,right-left+1);
             map[s[right]]=right;
         }
-        return maxi;
+
+        return len;
         
     }
 };
