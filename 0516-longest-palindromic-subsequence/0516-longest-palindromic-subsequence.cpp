3eea51f0a4f6fc86db9da404bdd1667196668ef3
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-26 12:10:40 +0530

    Time: 60 ms (65.2%), Space: 76 MB (45.23%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -12,14 +12,13 @@ public:
         for(int i=n-1;i>=0;i--){
             for(int j=i+1;j<n;j++){
                 if(s[i]==s[j]){
-                    dp[i][j]= 2+dp[i+1][j-1];
+                    dp[i][j]= 2+ dp[i+1][j-1];
                 }else{
                     dp[i][j]= max(dp[i+1][j],dp[i][j-1]);
                 }
             }
         }
         return dp[0][n-1];
-
         
     }
 };
