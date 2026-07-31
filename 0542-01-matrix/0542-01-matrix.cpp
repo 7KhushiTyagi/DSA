af6552d8a9a91d64736d10226550422d4ee33fab
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-31 15:39:41 +0530

    Time: 21 ms (46.12%), Space: 34.8 MB (66.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
 
-        vector<vector<int>> res(m,vector<int> (n,-1));
+        int m= mat.size();
+        int n= mat[0].size();
+
+        vector<vector<int>> graph(m,vector<int> (n,-1));
 
         queue<pair<int,int>> q;
+
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
+                    graph[i][j]=0;
                     q.push({i,j});
-                    res[i][j]=0;
                 }
             }
         }
 
         int delrow[]= {-1,0,1,0};
         int delcol[]= {0,1,0,-1};
+
 
         while(!q.empty()){
             int i= q.front().first;
@@ -27,17 +30,16 @@ public:
 
             for(int k=0;k<4;k++){
                 int row= delrow[k]+i;
-                int col= j+delcol[k];
+                int col= delcol[k]+j;
 
-                if(row<m && row>=0 && col<n && col>=0){
-                    if(res[row][col]==-1){
-                        q.push({row,col});
-                        res[row][col]= 1+res[i][j];
-                    }
+                if(row>=0 && col>=0 && row<m && col<n && graph[row][col]==-1){
+                    q.push({row,col});
+                    graph[row][col]= 1+graph[i][j];
                 }
             }
         }
-        return res;
+
+        return graph;
         
     }
 };
