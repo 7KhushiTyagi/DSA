50e9147c36fa68d999151d69837a11e29d0b4f19
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-07-31 15:53:17 +0530

    Time: 0 ms (100%), Space: 13.8 MB (91.05%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,16 +1,15 @@
 class Solution {
-public: 
-    void dfs(int i,int j,vector<vector<char>>& board,int delrow[],int delcol[],int m,int n){
+public:
+    void dfs(int m,int n,vector<vector<char>>& board,int delrow[],int delcol[],int i,int j){
+
         board[i][j]='#';
 
         for(int k=0;k<4;k++){
-            int row= delrow[k]+i;
-            int col= delcol[k]+j;
+            int row= i+ delrow[k];
+            int col= j+ delcol[k];
 
-            if(row<m && row>=0 && col<n && col>=0){
-                if(board[row][col]=='O'){
-                    dfs(row,col,board,delrow,delcol,m,n);
-                }
+            if(row>=0 && col>=0 && row<m && col<n && board[row][col]=='O'){
+                dfs(m,n,board,delrow,delcol,row,col);
             }
         }
     }
@@ -19,36 +18,41 @@ public:
         int m=board.size();
         int n=board[0].size();
 
-        int delrow[]= {0,1,0,-1};
-        int delcol[]= {1,0,-1,0};
+        int delrow[]= {-1,0,1,0};
+        int delcol[]= {0,1,0,-1};
 
         for(int i=0;i<m;i++){
             if(board[i][0]=='O'){
-              dfs(i,0,board,delrow,delcol,m,n);
+                dfs(m,n,board,delrow,delcol,i,0);
             }
             if(board[i][n-1]=='O'){
-                dfs(i,n-1,board,delrow,delcol,m,n);
+                dfs(m,n,board,delrow,delcol,i,n-1);
             }
+
         }
 
         for(int i=0;i<n;i++){
             if(board[0][i]=='O'){
-              dfs(0,i,board,delrow,delcol,m,n);
+                dfs(m,n,board,delrow,delcol,0,i);
             }
             if(board[m-1][i]=='O'){
-                dfs(m-1,i,board,delrow,delcol,m,n);
+                dfs(m,n,board,delrow,delcol,m-1,i);
             }
+
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]=='O'){
                     board[i][j]='X';
-                }else if (board[i][j] == '#') {
-                    board[i][j] = 'O'; 
+                }
+                else if(board[i][j]=='#'){
+                    board[i][j]='O';
                 }
             }
         }
+
+
         
     }
 };
