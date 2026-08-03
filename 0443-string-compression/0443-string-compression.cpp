5417fedd3b79b2a6617f5dab676400bc4346253e
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-08-03 13:25:46 +0530

    Time: 0 ms (100%), Space: 13.7 MB (76.34%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n=chars.size();
         int read=0;
         int write=0;
+        int n=chars.size();
 
         while(read<n){
             char curr= chars[read];
@@ -17,13 +17,13 @@ public:
             chars[write++]=curr;
 
             if(count>1){
-                string str= to_string(count);
-                for(char c:str){
+                string s= to_string(count);
+                for(char c:s){
                     chars[write++]=c;
                 }
             }
+
         }
         return write;
-        
     }
 };
