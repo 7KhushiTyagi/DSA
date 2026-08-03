c69002d81bb22bed73cf002d4ef3f2ddf868b510
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-08-03 11:54:45 +0530

    Time: 0 ms (100%), Space: 12 MB (25.81%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-
         sort(strs.begin(),strs.end());
+        int n=strs.size();
+        
         string res="";
-     
-        string first=strs[0];
-        string last=strs[strs.size()-1];
+        string first= strs[0];
+        string last= strs[n-1];
 
         for(int i=0;i<first.length();i++){
             if(first[i]==last[i]){
-                res+=first[i];
+                res+= first[i];
             }else{
                 break;
             }
         }
+
         return res;
+        
     }
 };
