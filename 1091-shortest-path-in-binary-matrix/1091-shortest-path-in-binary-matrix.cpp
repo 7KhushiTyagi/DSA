19128e400f93275643cd6c0cdbef6ca4ca67b3bc
Khushi Tyagi <khushityagi.7070@gmail.com>
2026-08-10 11:19:55 +0530

    Time: 26 ms (32.14%), Space: 21.6 MB (99.93%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
+        int m=grid.size();
+        int n= grid[0].size();
+
+        if(grid[0][0]==1 || grid[m-1][n-1]==1){
+            return -1;
+        }
+
+        priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        pq.push({1,{0,0}});
 
         int delrow[]= {-1,-1,0,1,1,1,0,-1};
         int delcol[]= {0,1,1,1,0,-1,-1,-1};
 
-        if(grid[0][0]!=0 || grid[n-1][n-1]!=0){
-            return -1;
-        }
-
-       priority_queue< pair<int,pair<int,int>>,
-       vector<pair<int,pair<int,int>>> ,
-       greater<pair<int,pair<int,int>>>> pq;
-        pq.push({1,{0,0}});
-        grid[0][0]=1;
-
         while(!pq.empty()){
-            int dis=pq.top().first;
-            int i=pq.top().second.first;
+            int dis= pq.top().first;
+            int i= pq.top().second.first;
             int j= pq.top().second.second;
             pq.pop();
 
-            if(i==n-1 && j==n-1){
+            if(i==m-1 && j==n-1){
                 return dis;
             }
 
@@ -30,19 +28,14 @@ public:
                 int row= delrow[k]+i;
                 int col= delcol[k]+j;
 
-                if(row<n && row>=0 && col<n && col>=0){
-                    if(grid[row][col]==0){
-                       grid[row][col]=1;
-                        pq.push({dis+1,{row,col}});
-                    }
+                if(row>=0 && row<m && col>=0 && col<n && grid[row][col]==0){
+                    grid[row][col]=1;
+                    pq.push({dis+1,{row,col}});
                 }
             }
         }
 
         return -1;
-        
-
-
         
     }
 };
